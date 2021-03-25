6e8ea2f459630d0337d66a1ee41f246a4903feb6
andypisz <a.piszyna@gmail.com>
2021-03-25 10:16:23 +0100

    feat: add header

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld :msg="message" :subtitle="subtitle"/>
-    <input type="text" v-model="subtitle" />
-    <button @click="generate"> Générer </button>
+    <NavBar/>
+    <main>
+      <HomePage :msg="message" />
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomePage from './components/HomePage.vue'
+import NavBar from './components/NavBar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomePage, NavBar
   },
   data(){
     return {
-      message: 'Quittance Generator',
-      subtitle: 'Génerez vos quittances facilement !'
+      message: 'Quittance Generator'
     }
   },
   methods: {
@@ -30,12 +30,18 @@ export default {
 </script>
 
 <style>
+body {
+  background: linear-gradient(to bottom, #fff, #ccc);
+  background-attachment: fixed;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+main {
+  margin: 0 auto;
+  padding: 30px;
+  background-color: white;
+  width: 1024px;
+  min-width: 300px;
 }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="header">
+    <header>
+      <nav>
+        <ul>
+          <li class="nav-item" >
+            Accueil
+          </li>
+          <li class="nav-item" >
+            Ajouter un bien
+          </li>
+          <li class="nav-item" >
+            Quittances générées
+          </li>
+        </ul>
+      </nav>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+header {
+  width: 1084px;
+  margin: 0 auto;
+}
+ul {
+  padding: 3px;
+  display: flex;
+}
+.nav-item {
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #00B0FF;
+}
+</style>
